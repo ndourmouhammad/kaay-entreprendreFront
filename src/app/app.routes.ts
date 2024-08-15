d33dac5d51e3927693b550cc47a81266bf08b37e
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-08-15 19:13:40 +0000

    l'intégration de la page de connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
+import { RegisterComponent } from './register/register.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent}
+    { path: 'dashboard-coach', component: DashboardCoachComponent},
+    { path: 'register', component: RegisterComponent}
     
 ];

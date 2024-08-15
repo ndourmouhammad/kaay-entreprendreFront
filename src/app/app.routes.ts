5da89cde59851b6da0c1d0db03f3eac4ebdff703
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-08-15 18:31:30 +0000

    Je me suis chargée de l'intégration de la page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent}
+    { path: 'dashboard-coach', component: DashboardCoachComponent},
+    { path: 'login', component: LoginComponent}
     
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [LoginComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  
+}

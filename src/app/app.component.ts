269a1db5950282b848e84e30a94bb0522cdfbb03
ANNA MARONE <annamarone72@gmail.com>
2024-08-16 09:06:41 +0000

    integration des deux header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header1Component } from './headerFooter/header1/header1.component';
+import { AccueilComponent } from './accueilcomponent/accueil/accueil.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Header1Component 
+  imports: [RouterOutlet,Header1Component,AccueilComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
 import { Header1Component } from './headerFooter/header1/header1.component';
+import { Header2Component } from './headerFooter/header2/header2.component';
+import { AccueilComponent } from './accueilcomponent/accueil/accueil.component';
+
+
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},
-    {path:'header1',component:Header1Component}
-    
+    {path:'header1',component:Header1Component},
+    {path:'header2',component:Header2Component},
+    {path:'pageAccueil',component:AccueilComponent},
 ];

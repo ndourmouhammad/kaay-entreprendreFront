1db61c3d9c4896664ca75c959cfd99a657829bea
ANNA MARONE <annamarone72@gmail.com>
2024-08-16 00:20:43 +0000

    header numéro1 avec le fond blanc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header1Component } from './headerFooter/header1/header1.component';
+import { Header2Component } from './headerFooter/header2/header2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Header1Component 
+  imports: [RouterOutlet,Header1Component,Header2Component 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/headerFooter/header2/header2.component.ts
+++ src/app/headerFooter/header2/header2.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-header2',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './header2.component.html',
   styleUrl: './header2.component.css'
 })

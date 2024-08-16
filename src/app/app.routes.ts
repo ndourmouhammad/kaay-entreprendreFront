77eee5718ad1a8691f7dbaf7f2a97b024441c6a4
ANNA MARONE <annamarone72@gmail.com>
2024-08-16 09:21:23 +0000

    page accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coa
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
 import { Header1Component } from './headerFooter/header1/header1.component';
 import { Header2Component } from './headerFooter/header2/header2.component';
-import { AccueilComponent } from './AccueilComponent/accueil/accueil.component';
-AccueilComponent
+
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},

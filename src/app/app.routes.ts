476dea518c5acbecfd58299e192453cab4b56df6
ANNA MARONE <annamarone72@gmail.com>
2024-08-16 08:45:30 +0000

    integration des deux header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coa
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
 import { Header1Component } from './headerFooter/header1/header1.component';
 import { Header2Component } from './headerFooter/header2/header2.component';
-Header2Component 
+import { AccueilComponent } from './AccueilComponent/accueil/accueil.component';
+AccueilComponent
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},
     {path:'header1',component:Header1Component},
     {path:'header2',component:Header2Component }
+
 ];
--- src/app/headerFooter/header2/header2.component.ts
+++ src/app/headerFooter/header2/header2.component.ts
@@ -8,5 +8,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './header2.component.css'
 })
 export class Header2Component {
+// Déclarations des variables
+private hamburgerMenu: HTMLElement | null = null;
+private navbar: HTMLElement | null = null;
 
+ngOnInit(): void {
+  // Sélectionne le menu hamburger et la barre de navigation
+  this.hamburgerMenu = document.querySelector('.hamburger-menu');
+  this.navbar = document.querySelector('.navbar');
+
+  // Ajoute un écouteur d'événements pour le clic sur le menu hamburger
+  if (this.hamburgerMenu && this.navbar) {
+    this.hamburgerMenu.addEventListener('click', () => {
+      if (this.navbar) { 
+        this.navbar.classList.toggle('active');
+      }
+      if (this.hamburgerMenu) { 
+        this.hamburgerMenu.classList.toggle('open');
+      }
+    });
+  }
+}
 }

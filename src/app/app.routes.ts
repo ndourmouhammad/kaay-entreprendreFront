5cc51ead539f057e111a6aaa926e4c9e7dc93806
ANNA MARONE <annamarone72@gmail.com>
2024-08-17 19:05:01 +0000

    intégration du footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
 import { Header1Component } from './headerFooter/header1/header1.component';
+import { FooterComponent } from './headerFooter/footer/footer.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},
-    {path:'header1',component:Header1Component}
+    {path:'header1',component:Header1Component},
+    {path:'footer',component:FooterComponent}
+
     
 ];

da99ecc649825a8952c16281efed085ce83825f2
ndongba <ndongba01@gmail.com>
2024-08-17 18:56:32 +0000

    integration profil coach pas encore fini

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
+import { DemandecoachComponent } from './entrepreneur/demandecoach/demandecoach.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent}
-    
+    { path: 'dashboard-coach', component: DashboardCoachComponent},
+    { path: 'demande/profilcoach', component: DemandecoachComponent }
+
 ];
---
+++ src/app/entrepreneur/demandecoach/demandecoach.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demandecoach',
+  standalone: true,
+  imports: [],
+  templateUrl: './demandecoach.component.html',
+  styleUrl: './demandecoach.component.css'
+})
+export class DemandecoachComponent {
+
+}

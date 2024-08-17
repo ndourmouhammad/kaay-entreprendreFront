4ce39dd94141570a36d6989547a7e00eaa85b729
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-08-17 22:28:49 +0000

    intégration de la page détails évents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
 import { EvenementComponent } from './evenement/evenement.component';
+import { DetailEvenementComponent } from './detail-evenement/detail-evenement.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},
-    { path: 'evenement', component: EvenementComponent}
+    { path: 'evenement', component: EvenementComponent},
+    { path: 'detail_evenement', component: DetailEvenementComponent}
     
 ];
---
+++ src/app/detail-evenement/detail-evenement.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-evenement',
+  standalone: true,
+  imports: [
+    DetailEvenementComponent
+  ],
+  templateUrl: './detail-evenement.component.html',
+  styleUrl: './detail-evenement.component.css'
+})
+export class DetailEvenementComponent {
+
+}

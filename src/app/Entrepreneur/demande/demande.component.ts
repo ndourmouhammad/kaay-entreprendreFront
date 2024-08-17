55255a93ec79789dffb36ceb3352be3ae664a770
ndongba <ndongba01@gmail.com>
2024-08-17 13:04:18 +0000

    Integration de la page principale demande accompagnement

---
+++ src/app/Entrepreneur/demande/demande.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demande',
+  standalone: true,
+  imports: [],
+  templateUrl: './demande.component.html',
+  styleUrl: './demande.component.css'
+})
+export class DemandeComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
+import { DemandeComponent } from './Entrepreneur/demande/demande.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent}
-    
+    { path: 'dashboard-coach', component: DashboardCoachComponent},
+    { path:'demande', component:DemandeComponent}
+
 ];

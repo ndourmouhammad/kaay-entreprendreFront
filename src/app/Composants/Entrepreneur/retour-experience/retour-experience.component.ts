f31e41af45de5eb6ad57684081c3b6d1537c0861
ndongba <ndongba01@gmail.com>
2024-08-18 11:55:37 +0000

    integration page retour experience user pas encore terminée

---
+++ src/app/Composants/Entrepreneur/retour-experience/retour-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retour-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './retour-experience.component.html',
+  styleUrl: './retour-experience.component.css'
+})
+export class RetourExperienceComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
+import { RetourExperienceComponent } from './Composants/Entrepreneur/retour-experience/retour-experience.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent}
-    
+    { path: 'dashboard-coach', component: DashboardCoachComponent},
+    { path: 'retourexperience', component: RetourExperienceComponent}
+
 ];

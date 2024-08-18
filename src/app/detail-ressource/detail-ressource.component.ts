9c3349f054a1c066e6f2fe30981bc85472ffe71c
ANNA MARONE <annamarone72@gmail.com>
2024-08-18 12:51:10 +0000

    resposivté de pa page détail ressource

--- src/app/detail-ressource/detail-ressource.component.ts
+++ src/app/detail-ressource/detail-ressource.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header1Component } from '../headerFooter/header1/header1.component';
 import { Header2Component } from '../headerFooter/header2/header2.component';
+import { FooterComponent } from '../headerFooter/footer/footer.component';
 
 @Component({
   selector: 'app-detail-ressource',
   standalone: true,
-  imports: [ RouterOutlet,Header1Component,Header2Component],
+  imports: [ RouterOutlet,Header1Component,Header2Component,FooterComponent],
   templateUrl: './detail-ressource.component.html',
   styleUrl: './detail-ressource.component.css'
 })

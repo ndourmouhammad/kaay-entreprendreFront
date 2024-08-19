82051aecb7937ee55f449d9ef160425b071670ac
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-19 13:54:38 +0000

    Merge de la branche retour d'expérience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
-import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
-import { RetourExperienceComponent } from './Composants/Entrepreneur/retour-experience/retour-experience.component';
+import { RetourExperienceComponent } from './Composants/Visiteur/retour-experience/retour-experience.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent},
     { path: 'retourexperience', component: RetourExperienceComponent}
-
 ];

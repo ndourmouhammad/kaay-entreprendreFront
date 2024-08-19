b87b1e075c93a771e5d805fabcef4710ea9c62f3
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-19 11:25:22 +0000

    Reglages de conflits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,7 @@
 import { Routes } from '@angular/router';
-import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
-import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
-import { Header1Component } from './headerFooter/header1/header1.component';
-import { FooterComponent } from './headerFooter/footer/footer.component';
-import { DetailRessourceComponent } from './detail-ressource/detail-ressource.component';
-import { VideoComponent } from './video/video.component';
+import { DetailRessourceComponent } from './Composants/Visiteur/detail-ressource/detail-ressource.component';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent},
-    {path:'header1',component:Header1Component},
-    {path:'footer',component:FooterComponent},
     {path:'detail',component:DetailRessourceComponent},
-    {path:'vidéo',component:VideoComponent}
-
-    
 ];

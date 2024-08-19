324fdd2e33a8e43725673e29792702d370207704
ndongba <ndongba01@gmail.com>
2024-08-19 11:39:28 +0000

    integration page profil coach terminée

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
 import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
-import { DemandecoachComponent } from './entrepreneur/demandecoach/demandecoach.component';
+import { DemandecoachComponent } from './Composants/Entrepreneur/demandecoach/demandecoach.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
     { path: 'dashboard-coach', component: DashboardCoachComponent},
-    { path: 'demande/profilcoach', component: DemandecoachComponent }
+    { path: 'demande/profilcoach' , component: DemandecoachComponent}
 
 ];

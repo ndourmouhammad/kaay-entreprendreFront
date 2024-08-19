3f08e54810987309511f829c4513ec956c1af177
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-19 13:34:16 +0000

    Récupération de la branche profil demande de coach

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { HeaderCoachComponent } from './Composants/Coach/header-coach/header-coach.component';
-import { DashboardCoachComponent } from './Composants/Coach/dashboard-coach/dashboard-coach.component';
-import { DemandecoachComponent } from './Composants/Entrepreneur/demandecoach/demandecoach.component';
+
+import { DemandecoachComponent } from './Composants/Visiteur/demandecoach/demandecoach.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'visiteur', pathMatch: 'full' },
-    { path: 'dashboard-coach', component: DashboardCoachComponent},
-    { path: 'demande/profilcoach' , component: DemandecoachComponent}
+    
+    { path: 'liste-demande' , component: DemandecoachComponent}
 
 ];

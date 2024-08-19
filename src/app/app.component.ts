e35b27b3c89a3ea6c5c2d31c41ff7daa1dd3788f
ANNA MARONE <annamarone72@gmail.com>
2024-08-19 15:03:53 +0000

    restution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header1Component } from './headerFooter/header1/header1.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Header1Component 
+  imports: [RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

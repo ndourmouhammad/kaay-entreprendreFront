85576c62319a02e51640f286276500a4cfc31b72
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-23 10:56:59 +0000

    Dynamiser la barre de navigation

--- src/app/Composants/Admin/header-admin/header-admin.component.ts
+++ src/app/Composants/Admin/header-admin/header-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../../Services/auth.service';
 
@@ -7,15 +7,24 @@ import { AuthService } from '../../../Services/auth.service';
   standalone: true,
   imports: [
     RouterModule,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header-admin.component.html',
   styleUrl: './header-admin.component.css'
 })
-export class HeaderAdminComponent {
+export class HeaderAdminComponent implements OnInit {
+
   // Injection des dépendances
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  userName: string | null = '';
+
+  ngOnInit(): void {
+      const user = JSON.parse(localStorage.getItem('user') || '{}');
+      this.userName = user?.name || 'Utilisateur';
+  }
 
   logout() {
     return this.authService.logout().subscribe(
--- src/app/Composants/Coach/profil-coach/profil-coach.component.ts
+++ src/app/Composants/Coach/profil-coach/profil-coach.component.ts
@@ -1,15 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { HeaderCoachComponent } from '../header-coach/header-coach.component';
+import { UserModel } from '../../../Models/users.model';
+import { UsersService } from '../../../Services/users.service';
+import { NgIf } from '@angular/common';
+import { environment } from '../../../../environnements/environments';
+import { throwError, catchError } from 'rxjs';
+import { RouterLink, RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profil-coach',
   standalone: true,
   imports: [
-    HeaderCoachComponent
+    HeaderCoachComponent,
+    NgIf,
+    RouterLink,
+    RouterModule
   ],
   templateUrl: './profil-coach.component.html',
   styleUrl: './profil-coach.component.css'
 })
-export class ProfilCoachComponent {
+export class ProfilCoachComponent implements OnInit {
 
+    // Injection des dépendances
+    private usersService = inject(UsersService);
+    private router = inject(Router);
+
+    user: UserModel = {};
+    baseUrl: string = environment.apiUrl;
+    photoUrl: string = '';
+
+    ngOnInit(): void {
+        // Afficher les informations de l'utilisateur connecté
+        this.getMe();
+    }
+
+    // Afficher les informations de l'utilisateur connecté
+    getMe(): void {
+      this.usersService.getMe().pipe(
+          catchError(error => {
+              console.error('Erreur lors de la récupération de l\'utilisateur connecté:', error);
+              return throwError(error);
+          })
+      ).subscribe(
+          response => {
+              console.log('Réponse de l\'API:', response);
+              if (response && response.user) {
+                  if (response.user !== null) {
+                      this.user = response.user;
+                      this.photoUrl = `${this.baseUrl}${this.user.photo}`;
+                  } else {
+                      console.warn('Aucun utilisateur trouvé.');
+                      this.user = {
+                          name: 'Utilisateur non trouvé',
+                          telephone: 'Non spécifié',
+                          email: 'Non spécifié',
+                          adresse: 'Non spécifié',
+                          photo: ''
+                      };
+                  }
+              } else {
+                  console.warn('Données utilisateur manquantes dans la réponse');
+                  this.user = {
+                      name: 'Utilisateur inconnu',
+                      telephone: 'Non spécifié',
+                      email: 'Non spécifié',
+                      adresse: 'Non spécifié',
+                      photo: ''
+                  };
+              }
+          },
+          error => {
+              console.error('Erreur lors de la récupération de l\'utilisateur connecté:', error);
+          }
+      );
+  }
+
+  getPhotoUrl(photoPath: string): string {
+    return `${this.baseUrl}${photoPath}`;
+  }
+  
 }

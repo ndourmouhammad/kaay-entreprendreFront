4a00a9ead5adfbfaf7896b6268207fdf1682492a
ANNA MARONE <annamarone72@gmail.com>
2024-08-26 17:34:57 +0000

    amélioration des pages

--- src/environnements/environments.ts
+++ src/environnements/environments.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-     apiUrl: 'http://localhost:8000/storage/',
+     apiUrl: 'http://localhost:8000/storage',
     //apiUrl: 'https://kaayentreprendre.ndourmouhammad15.simplonfabriques.com/storage/'
     
     

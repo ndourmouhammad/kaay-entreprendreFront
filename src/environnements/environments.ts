ca178f2e20190e31a8a5055e37ffce1e8af104fd
Mouhammad Ndour <123220695+ndourmouhammad@users.noreply.github.com>
2024-09-01 14:09:51 +0000

    Update environments.ts

--- src/environnements/environments.ts
+++ src/environnements/environments.ts
@@ -1,8 +1,8 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-     apiUrl: 'http://localhost:8000/storage/',
-    // apiUrl: 'https://kaayentreprendre.ndourmouhammad15.simplonfabriques.com/storage/'
+     //apiUrl: 'http://localhost:8000/storage/',
+    apiUrl: 'https://kaayentreprendre.ndourmouhammad15.simplonfabriques.com/storage/'
     
     
   };

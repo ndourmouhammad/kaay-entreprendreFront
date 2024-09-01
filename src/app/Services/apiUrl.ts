e89a5d4e4788bd284bf0d112f00d6cdff828fe07
Mouhammad Ndour <123220695+ndourmouhammad@users.noreply.github.com>
2024-09-01 14:08:58 +0000

    Update apiUrl.ts

--- src/app/Services/apiUrl.ts
+++ src/app/Services/apiUrl.ts
@@ -1,3 +1,3 @@
-export const apiUrl = "http://127.0.0.1:8000/api/";
+//export const apiUrl = "http://127.0.0.1:8000/api/";
 
-// export const apiUrl = "https://kaayentreprendre.ndourmouhammad15.simplonfabriques.com/api/";
+export const apiUrl = "https://kaayentreprendre.ndourmouhammad15.simplonfabriques.com/api/";
